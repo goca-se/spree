68f836dfeb5c7cc75a47467ff70c11f965b6ad72
Isabella Santos <isabella.santos@gocase.com.br>
2025-02-17 17:08:31 -0300

    fix: add ApplicationRecord

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -89,8 +89,7 @@ module Spree
           Spree::Promotion::Rules::UserLoggedIn,
           Spree::Promotion::Rules::OneUsePerUser,
           Spree::Promotion::Rules::Taxon,
-          Spree::Promotion::Rules::OptionValue,
-          Spree::Promotion::Rules::Country
+          Spree::Promotion::Rules::OptionValue
         ]
       end
 
